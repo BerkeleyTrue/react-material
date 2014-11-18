3a60fa09de582336a3d5e7a811c9771bc181d70f
Berkeley Martinez <Berkeley@robotie.com>
2014-11-18 07:09:44 -0800

    Hides unfinished work from the public.

--- views/TextFieldsView.js
+++ views/TextFieldsView.js
@@ -16,16 +16,16 @@ var TextFieldsView = React.createClass({
       <TextField placeHolder="Name"/>
       <h2>Floating Labels</h2>
       <TextField placeHolder="Surname" floatingLabel={true}/>
-      <h2>Multi-line-text field</h2>
-    TODO
+      {/*<h2>Multi-line-text field</h2>
+      TODO
       <h2>Full-width text field</h2>
-    TODO
+      TODO
       <h2>Character Counter</h2>
-    TODO
+      TODO
       <h2>Auto-complete text field</h2>
-    TODO
+      TODO
       <h2>Search Filter</h2>
-    TODO
+      TODO*/}
       <h2>Error indication</h2>
       <TextField error="Well that sucks" />
     </div>;
